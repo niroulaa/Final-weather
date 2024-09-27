e26b879148945ef8d4345c40221c1b6154403c67
Rachit Raj Niroula <160651882+Krakhumnaam@users.noreply.github.com>
2024-09-27 19:47:52 +0545

    good

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import Mainbody from './Component/Mainbody';
+import "./App.css";
+import Navbar from "./Component/Navbar";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className="App h-[100vh] overflow-hidden " >
+      <Navbar />
+      <Mainbody />
+     
+    
+     </div>
   );
 }
 
